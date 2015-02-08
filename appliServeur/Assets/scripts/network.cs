32773fa7f4bec7d3737197d6a7a11afe595e34c5
HUANGManutea <manutea.huang@insa-rennes.fr>
2015-02-08 12:17:06 +0100

    creating serverApp

--- appliServeur/Assets/scripts/network.cs
+++ appliServeur/Assets/scripts/network.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class network : MonoBehaviour {
-
-	public int nbCo = 4;
+public class NetworkManager : MonoBehaviour {
+	
+	public string ipAddress;
 	public int port = 25565;
-	// Use this for initialization
-	void Start () {
-		Network.InitializeServer(nbCo,port,false);
+	public int nbClient = 2;
+	public Transform spawnServer;
+	
+	//GUI
+	void OnGUI(){
+		if(GUI.Button(new Rect(250,130,125,25),"Créer un serveur")){
+			Network.InitializeServer(nbClient,port,false);
+		}
 	}
-
+	
+	//messages
 	void OnPlayerConnected(){
-		Debug.Log("une personne s'est connectée");
+		Debug.Log("client connecté");
+	}
+	void OnConnectedToServer(){
+		Debug.Log("Connecté");
+	}
+	
+	void OnFailedToConnect(){
+		Debug.Log("Impossible de se connecter à: "+ ipAddress);
+	}
+	
+	void OnServerInitialized(){
+		Debug.Log("Serveur crée");
 	}
 }
