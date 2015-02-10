c11c54520a2700973ffb95e8238f40c75d04a3b3
Zeldarck <zeldarck.martin@gmail.com>
2015-02-10 18:16:54 +0100

    Commencement d'organisation de potato
    Ajout des panels principaux
    Ajout du Draw_menu et des script liées
    => pour récupéré les infos du Draw_menu, voir le script Image dans le
    dossier du Draw_menu

---
+++ CraftingApp/Assets/Draw_menu/Draw_menu.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Draw_menu : MonoBehaviour {
+
+	private const int COULEUR = 0;
+	private const int TOOLS = 1;
+	private const int DIAM = 2;
+
+	public enum Tools {PENCIL,ERASER,BUCKET};
+	public enum Diam {SMALL = 5,MEDIUM = 10,LARGE = 20};
+
+	public Color color;
+	public Tools tool;
+	public Diam diam;
+
+
+	public UnityEngine.UI.Button colorSelect;
+	public UnityEngine.UI.Button toolSelect;
+	public UnityEngine.UI.Button diamSelect;
+
+	public void Start(){
+		setBlack ();
+		setPencil ();
+		setMedium ();
+	}
+
+	public void selectionne(string c,int type){
+
+		GameObject bouton = GameObject.Find (c);
+
+		switch (type) {
+				case COULEUR:
+						if (colorSelect != null) {
+								colorSelect.interactable = true;
+						}
+
+						
+						if (bouton != null) {
+								colorSelect = bouton.GetComponent<UnityEngine.UI.Button> ();
+						}
+		
+
+						colorSelect.interactable = false;
+				
+							break;
+				case TOOLS:
+					if (toolSelect != null) {
+						toolSelect.interactable = true;
+					}
+					
+					if (bouton != null) {
+						toolSelect = bouton.GetComponent<UnityEngine.UI.Button> ();
+					}
+					
+					
+					toolSelect.interactable = false;
+					
+					break;
+
+				case DIAM:
+					if (diamSelect != null) {
+						diamSelect.interactable = true;
+					}
+					
+					if (bouton != null) {
+						diamSelect = bouton.GetComponent<UnityEngine.UI.Button> ();
+					}
+					
+					
+					diamSelect.interactable = false;
+					
+					break;
+				}
+		}
+
+
+	//Selection des couleurs
+
+	public void setRed(){
+		color = Color.red;
+
+		selectionne ("red",COULEUR);
+
+		}
+
+	public void setBlue(){
+		color = Color.blue;
+
+		selectionne ("blue",COULEUR);
+	}
+
+	public void setGreen(){
+		color = Color.green;
+
+		selectionne ("green",COULEUR);
+	}
+
+	public void setYellow(){
+		color = Color.yellow;
+
+		selectionne ("yellow",COULEUR);
+	}
+
+	public void setBlack(){
+		color = Color.black;
+
+		selectionne ("black",COULEUR);
+	}
+
+	public void setCyan(){
+		color = Color.cyan;
+
+		selectionne ("cyan",COULEUR);
+	}
+
+	public void setMagenta(){
+		color = Color.magenta;
+		
+		selectionne ("magenta",COULEUR);
+	}
+
+	public void setWhite(){
+		color = Color.white;
+		
+		selectionne ("white",COULEUR);
+	}
+
+	//Selection des outils
+	
+	public void setPencil(){
+		tool = Tools.PENCIL;
+		
+		selectionne ("pencil",TOOLS);
+		
+	}
+	public void setEraser(){
+		tool = Tools.ERASER;
+		
+		selectionne ("eraser",TOOLS);
+		
+	}
+	public void setBucket(){
+		tool = Tools.BUCKET;
+		
+		selectionne ("bucket",TOOLS);
+		
+	}
+
+	//Selection diamètre
+	public void setSmall(){
+		diam = Diam.SMALL;
+		
+		selectionne ("small",DIAM);
+		
+	}
+	public void setMedium(){
+		diam = Diam.MEDIUM;
+		
+		selectionne ("medium",DIAM);
+		
+	}
+	public void setLarge(){
+		diam = Diam.LARGE;
+		
+		selectionne ("large",DIAM);
+		
+	}
+
+
+
+	public Color getColor(){
+				return color;
+		}
+	public Tools getTool(){
+		return tool;
+	}
+	public Diam getDiam(){
+		return diam;
+	}
+}
---
+++ CraftingApp/Assets/Draw_menu/Image.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Image : MonoBehaviour {
+	public UnityEngine.UI.Image go;
+	public Draw_menu dm;
+	// Use this for initialization
+	void Start () {
+		//GetComponent<UnityEngine.UI.Image>().color = Color.blue;	
+		go.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		go.color = dm.getColor ();
+	}
+}
