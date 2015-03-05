f7a5ca6aa55c6fad1cd1240e25417ef65ae56eaa
Zeldarck <zeldarck.martin@gmail.com>
2015-03-05 13:46:41 +0100

    Ajout d'une camera preview
    Mouvement horizontaux possibles
    reflexion pour les verticaux

---
+++ CraftingApp/Assets/Cam/CameraDrag.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+	public float dragSpeed = 8;
+	public GameObject go;//objet autour duquel on veut tourner
+	
+	
+	void Update()
+	{
+		
+		
+		if (Input.GetMouseButton(0)) {
+			float origine = Input.GetAxis("Mouse X"); 
+			//float origine2 = Input.GetAxis("Mouse Y"); 
+			//je ne sais pas vraiment comment marche cette ligne, mais en tout cas ça fonctionne
+			
+			transform.RotateAround (go.transform.position, Vector3.up, origine*dragSpeed);
+			//transform.RotateAround (go.transform.position, Vector3.left, origine2*dragSpeed);
+			//fonction pour tourner autour d'un point précis
+		}
+		
+	}
+	
+	
+}
