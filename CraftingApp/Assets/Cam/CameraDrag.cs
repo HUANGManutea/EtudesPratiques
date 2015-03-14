64ffd1a16908b5fe5ed8710b6196b4f858fd445f
Zeldarck <zeldarck.martin@gmail.com>
2015-03-14 15:46:51 +0100

    Restriction position souris pour cam

--- CraftingApp/Assets/Cam/CameraDrag.cs
+++ CraftingApp/Assets/Cam/CameraDrag.cs
@@ -4,14 +4,28 @@ public class CameraDrag : MonoBehaviour
 {
 	public float dragSpeed = 8;
 	public GameObject go;//objet autour duquel on veut tourner
-	
-	
+	public float X; //Viewport Rec de la cam
+	public float Y;
+	public float W;
+	public float H;
+	private int largeur;
+	private int hauteur;
+
+	void Start(){
+			largeur = Screen.width;
+			hauteur = Screen.height;
+		}
+
 	void Update()
 	{
+		Vector3 pos = Input.mousePosition;
+
+
 		
-		
-		if (Input.GetMouseButton(0)) {
-			float origine = Input.GetAxis("Mouse X"); 
+		if (Input.GetMouseButton(0) && (pos.x > X*largeur && pos.x < W*largeur) && 
+		    	(pos.y > Y*hauteur && pos.y < H*hauteur)) {
+			float origine = Input.GetAxis("Mouse X");
+
 			//float origine2 = Input.GetAxis("Mouse Y"); 
 			//je ne sais pas vraiment comment marche cette ligne, mais en tout cas ça fonctionne
 			
