c36824b2161c491fd296c0e0da691702ab38e7f9
Zeldarck <zeldarck.martin@gmail.com>
2015-03-21 13:12:16 +0100

    Redimensionnement qui foire

--- CraftingApp/Assets/Redimension.cs
+++ CraftingApp/Assets/Redimension.cs
@@ -8,25 +8,71 @@ public class Redimension : MonoBehaviour {
 	public float H;
 	private int largeurEcran;
 	private int hauteurEcran;
-	private float largeur;
-	private float hauteur;
+	private int largeur;
+	private int hauteur;
+	public GameObject myobject;
 	
 	// Use this for initialization
 	void Start () {
 		largeurEcran = Screen.width;
 		hauteurEcran = Screen.height;
 		
-		largeur = largeurEcran * (W - X);
-		hauteur = hauteurEcran * (H - Y);
+		largeur = (int)( largeurEcran * (W - X));
+		hauteur = (int)(hauteurEcran * (H - Y));
 		
-		Vector3 newScale = new Vector3(largeur, 1, hauteur); // Les dimensions souhaités
-		transform.localScale = newScale;
+		//Vector3 newScale = new Vector3(largeur, 1, hauteur); // Les dimensions souhaités
+		//transform.localScale = newScale;
+
+		float currentSize = myobject.renderer.bounds.size.x+ (float)95;
 		
+		Vector3 scale = myobject.transform.localScale;
+
+		print (currentSize);
+		print (scale);
+
+		scale.x = largeur * scale.x / currentSize;
+
+
+		currentSize = myobject.renderer.bounds.size.y + (float)65; 
+		print (currentSize);
+		print (scale);
+		
+		scale.z = hauteur * scale.z / currentSize;
+		
+		myobject.transform.localScale = scale;
+
+		//((Texture2D) (myobject.renderer.material.mainTexture)).Resize(largeur, hauteur);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		/*largeurEcran = Screen.width;
+		hauteurEcran = Screen.height;
 		
+		largeur = (int)( largeurEcran * (W - X));
+		hauteur = (int)(hauteurEcran * (H - Y));
+		
+		//Vector3 newScale = new Vector3(largeur, 1, hauteur); // Les dimensions souhaités
+		//transform.localScale = newScale;
+		
+		float currentSize = myobject.renderer.bounds.size.x+ (float)95;
+		
+		Vector3 scale = myobject.transform.localScale;
+		
+		print (currentSize);
+		print (scale);
+		
+		scale.x = largeur * scale.x / currentSize;
+		
+		
+		currentSize = myobject.renderer.bounds.size.y + (float)65; 
+		print (currentSize);
+		print (scale);
+		
+		scale.z = hauteur * scale.z / currentSize;
+		
+		myobject.transform.localScale = scale;
+*/
 	}
 }
