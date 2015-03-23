717df98bd2b4e1b90c915f4fa8a98ef5ef40ece6
Zeldarck <zeldarck.martin@gmail.com>
2015-03-23 16:25:56 +0100

    Ajout d'une caméra qui suis une direction
    Méthode pour changer l'axe est présente
    Manquerai juste la fonction pour changer la taille de la cam, à voir si
    utile

---
+++ CraftingApp/Assets/FollowObject.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//à mettre sur une caméra en projection orthographique
+
+public class FollowObject : MonoBehaviour {
+	Transform target;
+	public enum Direction {X,Y,Z};
+	private Direction dir;
+	private string name = "Exemple1";
+	
+	void Start () {
+		target = GameObject.Find(name).transform;
+		dir = Direction.X;
+	}
+	
+	void setName(string s){
+		name = s;
+	}
+	
+	void setDir(char s){
+		switch (s) {
+		case 'X':
+			dir = Direction.X;
+			break;
+		case 'Y':
+			dir = Direction.Y;
+			break;
+		case 'Z':
+			dir = Direction.Z;
+			break;
+		}
+		
+	}
+	
+	
+	void Update () {
+		
+		switch(dir){
+			
+		case Direction.X:
+			transform.rotation = Quaternion.LookRotation (target.right);
+			transform.position = new Vector3 (target.position.x, target.position.y,target.position.z);
+			
+			for (int i =0; i<10; i++) {
+				transform.Translate(Vector3.left, target);
+			}
+			break;
+			
+		case Direction.Y:
+			transform.rotation = Quaternion.LookRotation (target.up);
+			transform.position = new Vector3 (target.position.x, target.position.y,target.position.z);
+			
+			for (int i =0; i<10; i++) {
+				transform.Translate(Vector3.down, target);
+			}
+			break;
+			
+		case Direction.Z:
+			transform.rotation = Quaternion.LookRotation (target.forward);
+			transform.position = new Vector3 (target.position.x, target.position.y,target.position.z);
+			
+			for (int i =0; i<10; i++) {
+				transform.Translate(Vector3.back, target);
+			}
+			break;
+		}
+	}
+}
