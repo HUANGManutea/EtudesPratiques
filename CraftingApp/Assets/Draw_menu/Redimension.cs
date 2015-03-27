6fc745f0034b533497eb109cad5da53b00bad7da
Zeldarck <zeldarck.martin@gmail.com>
2015-03-27 17:13:37 +0100

    changement du z des menu
    Problème pour le placement de drawmenu qui désactive Draw Area si sont
    z < -19

--- CraftingApp/Assets/Draw_menu/Redimension.cs
+++ CraftingApp/Assets/Draw_menu/Redimension.cs
@@ -24,32 +24,12 @@ public class Redimension : MonoBehaviour {
 		
 		largeurEcran = Screen.width;
 		hauteurEcran = Screen.height;
-		Debug.Log("aaaaaaaaaa " + largeurEcran + " " + hauteurEcran);
 		
 		largeur =  largeurEcran * (W-X);
 		hauteur = hauteurEcran * (H-Y);
-		print (largeur);
 
 		gameObject.transform.localScale = new Vector3(largeur/10, 1, hauteur/10);
-
-		//le problème doit venir du résultat rendu ici, qui est inférieur à la valeur souhaiter
-		/*double currentSize =  myobject.renderer.bounds.size.x;
 		
-		Vector3 scale = myobject.transform.localScale;
-		
-		print (currentSize);
-		print (scale);
-		
-		scale.x = (float) (largeur * scale.x / currentSize);
-		
-		
-		currentSize =  myobject.renderer.bounds.size.y ; 
-		print (currentSize);
-		print (scale);
-		
-		scale.z = (float)( hauteur * scale.z / currentSize);
-		
-		myobject.transform.localScale = scale;*/
 
 	}
 }
