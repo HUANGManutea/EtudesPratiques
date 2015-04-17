cdacd5323cb814311fb53524150cbd6f4b2eec09
HUANGManutea <manutea.huang@insa-rennes.fr>
2015-04-17 17:41:53 +0200

    gestion ip

---
+++ CraftingApp/Assets/Standard Assets (Mobile)/Scripts/getInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class getInput : MonoBehaviour {
+	public GameObject textfield;
+	public static string ip;
+
+	void Start(){
+		ip = textfield.GetComponent<InputField>().text;
+		Debug.Log(ip);
+	}
+}
