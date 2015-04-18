ac07ccc843f3622fc7393f4262710cd30b02322d
Zeldarck <zeldarck.martin@gmail.com>
2015-04-18 14:13:02 +0200

    preview cam sur tactile normalement réglé

--- CraftingApp/Assets/Cam/CameraDrag.cs
+++ CraftingApp/Assets/Cam/CameraDrag.cs
@@ -1,49 +1,51 @@
 ﻿using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
-{
+{	
+	//vitesse de rotation
 	public float dragSpeed = 8;
-	public GameObject go;//objet autour duquel on veut tourner
-	public float X; //Viewport Rec de la cam
+
+	//objet autour duquel on veut tourner
+	public GameObject go;
+
+	//coordonnées du coin supérieur gauche
+	public float X; 
 	public float Y;
+	//coordonnées du coin inférieur droit
 	public float W;
 	public float H;
+
+	//taille de l'écran
 	private int largeur;
 	private int hauteur;
 
 	void Start(){
 			largeur = Screen.width;
-			hauteur = Screen.height;
-			
+			hauteur = Screen.height;	
 		}
 
 	void Update()
 	{
-		Vector3 pos = Input.mousePosition;
+		//si on appuie
+		if (Input.GetMouseButton(0)){
+			Vector3 pos = Input.mousePosition;
 
-		if (Input.GetMouseButton(0) && (pos.x > X*largeur && pos.x < W*largeur) && 
-		    	(pos.y > Y*hauteur && pos.y < H*hauteur)) {
-			float origine = Input.GetAxis("Mouse X");
+		 //si la souris est dans l'écran
+		 if( (pos.x > X*largeur && pos.x < W*largeur) && (pos.y > Y*hauteur && pos.y < H*hauteur)) {
 
-			float origine2 = Input.GetAxis("Mouse Y"); 
+				//on récupère le déplacement
+				float origine = Input.GetAxis("Mouse X");
+				float origine2 = Input.GetAxis("Mouse Y"); 
 
-			transform.RotateAround (go.transform.position, Vector3.up, origine*dragSpeed);
-			transform.Translate (Vector3.forward * origine2 * 5, transform);
+				//on tourne autour pour le déplacement selon X
+				transform.RotateAround (go.transform.position, Vector3.up, origine*dragSpeed);
+				//on zoom/dezoom pour le déplacement selon Y
+				transform.Translate (Vector3.forward * origine2 * 5, transform);
+			}
 		}
-		//Pos ();
-		
+
 	}
 
-	void Pos(){
-		Renderer[] child = go.GetComponentsInChildren<Renderer> ();
-		float max = child[0].bounds.max.x;
-		float min = child[0].bounds.min.x;
-		foreach(Renderer o in child){
-			max = Mathf.Max(max,o.bounds.max.x);
-			min = Mathf.Min(min,o.bounds.min.x);
-		}
-		print (max +"  " + min);
-	}
 	
 	
 }
